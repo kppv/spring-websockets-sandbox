ce2b607f0e895fa5cdfcb6ba653c4ab13a4aba09
Pavel Kurdyukov <kurdyukov.pavel@gmail.com>
2018-11-24 00:18:59 +0300

    just for travis

---
+++ src/test/java/ru/sandbox/TravisTest.java
@@ -0,0 +1,13 @@
+package ru.sandbox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TravisTest {
+    @Test
+    void travisTest() {
+        int x = 1;
+        assertEquals(1, x);
+    }
+}
